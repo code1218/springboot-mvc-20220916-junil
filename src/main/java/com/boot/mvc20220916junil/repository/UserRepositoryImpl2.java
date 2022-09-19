f42317533dbf6a025f6c6ca65f0610cc33ee2fe7
code1218 <skjil1218@gmail.com>
2022-09-19 12:51:59 +0900

    DI and IoC PathVariable DTO Entity

--- src/main/java/com/boot/mvc20220916junil/repository/UserRepositoryImpl2.java
+++ src/main/java/com/boot/mvc20220916junil/repository/UserRepositoryImpl2.java
@@ -7,11 +7,11 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 
-@Repository("a")
-public class UserRepositoryImpl implements UserRepository {
+@Repository("b")
+public class UserRepositoryImpl2 implements UserRepository {
     private final List<User> userData;
 
-    public UserRepositoryImpl() {
+    public UserRepositoryImpl2() {
         userData = new ArrayList<User>();
 
         for(int i = 0; i < 5; i++) {
@@ -33,22 +33,7 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public int save(User user) {
-        try {
-            int maxCode = 0;
-            for(User u : userData){
-                if(u.getUser_code() > maxCode) {
-                    maxCode = u.getUser_code();
-                }
-            }
-            maxCode++;
-
-            user.setUser_code(maxCode);
-
-            userData.add(user);
-        }catch (Exception e) {
-            return 0;
-        }
-        return 1;
+        return 0;
     }
 
     @Override
@@ -56,7 +41,7 @@ public class UserRepositoryImpl implements UserRepository {
         User user = null;
 
         for(User userObj: userData) {
-            if (userObj.getUser_id().equals(userId)) {
+            if (userObj.getUser_id().equals("userId")) {
                 user = userObj;
             }
         }
--- src/main/java/com/boot/mvc20220916junil/web/controller/api/UserRestController.java
+++ src/main/java/com/boot/mvc20220916junil/web/controller/api/UserRestController.java
@@ -1,26 +1,51 @@
 package com.boot.mvc20220916junil.web.controller.api;
 
+import com.boot.mvc20220916junil.domain.User;
 import com.boot.mvc20220916junil.repository.UserRepository;
+import com.boot.mvc20220916junil.web.dto.UserAddReqDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/v1")
-@RequiredArgsConstructor
 public class UserRestController {
 
-    private final UserRepository userRepository;
+    @Autowired
+    @Qualifier("a")
+    private UserRepository userRepository;
 
     @GetMapping("/users/{userCode}")
-    public ResponseEntity<?> getUser() {
+    public ResponseEntity<?> getUser(@PathVariable int userCode) {
 
-        return ResponseEntity.ok().body(null);
+        User user = userRepository.findUserByUserCode(userCode);
+
+        return ResponseEntity.ok().body(user);
     }
+
+    @GetMapping("/users2/{userId}")
+    public ResponseEntity<?> getUser(@PathVariable String userId) {
+        User user = userRepository.findUserByUserId(userId);
+
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<?> addUser(UserAddReqDto userAddReqDto) {
+        int result = userRepository.save(userAddReqDto.toEntity());
+        if(result == 0) {
+            return ResponseEntity.internalServerError().body("데이터 오류(Server)");
+        }
+        return ResponseEntity.ok().body("사용자 추가 완료");
+        
+    }
+
+
 
 
 }
